345c3852af21a1343c826c87b942d3044c5e7de9
srinivassridharan <srinivassridharan@ieee.org>
2012-03-20 13:17:07 -0700

    cleaned up code

--- fitForFnSq.m
+++ fitForFnSq.m
@@ -1,22 +1,16 @@
 function quadcell = fitForFnSq(fntofit, xhat, numberOfQuadratics, numOfPtsAroundEachQuadratic, rangeAroundXhat )
-% fitForFn(fntofit, xhat, numberOfQuadratics, numOfPtsAroundEachQuadratic, rangeAroundXhat );
-	opt1= optimset('Display','off');		
+%% this function returns a min-plus expansion of the square of the function fntofit around xhat. It does this by using a 
+% nonlinear constrained optimization on a grid of numOfPtsAroundEachQuadratic  around the center point of each
+% quadratic. The centerpoints of each quadratic are chosen by linearly choosing points in a window zone (rangeAroundXhat) 
+% about  xhat. 
+
+	opt1= optimset('Display','off');% it speeds up the run if messages arent being printed out. Comment this line during debugging		
 	quadcell = {};
-    xhat = xhat(2);
-%{
-	numberOfQuadratics = 7;
-	numOfPtsAroundEachQuadratic = 20;
-	rangeAroundXhat = 3;
-	%}
+  xhat = xhat(2); % in the example used only the 2nd state influences the output directly (since y = ((x_2) ^3)/40
 
 	xsamplePts = linspace(xhat - rangeAroundXhat, xhat + rangeAroundXhat, 30)';
 	fnvalatxsamplepts = fntofit(xsamplePts);
-% 	A = [ones(length(xsamplePts),1), xsamplePts, xsamplePts.^2];
-% 	fnewsamplepts= - 2*yval'*A;
-% 	Hsamplepts = 2*A'*A;
-		
 	centerPts = linspace(xhat-rangeAroundXhat, xhat + rangeAroundXhat, numberOfQuadratics + 1);
-%     keyboard;
 	for(k = 1:length(centerPts)-1)
 		xval = linspace(centerPts(k),centerPts(k+1),numOfPtsAroundEachQuadratic)';
 		yval = fntofit(xval);
@@ -24,55 +18,22 @@ function quadcell = fitForFnSq(fntofit, xhat, numberOfQuadratics, numOfPtsAround
 		f = -2*A'*yval;
 		fnew= f';
 		H = 2*A'*A;
-		% quadprog minimizes (1/2)*x'*H*x + f'*x
-% 		pval = fmincon(@(x) ((1/2)*x'*H*x + fnew*x), [150;80;160],[],[],[],[],[1;-250;-250],[],@(x)fnnonlincon(x,xsamplePts,fnvalatxsamplepts));
-        
-
-% for + - x^3/40
-%         pval = fmincon(@(x) ((1/2)*x'*H*x + fnew*x), [3/40;mean(diff(yval))/mean(diff(xval));mean(yval)],[],[],[],[],[.01;-250;-250],[],@(x)fnnonlincon(x,xsamplePts,fnvalatxsamplepts));
-
-        
-        
-        
-        
-     % for x^6/40^3  
-%      ver 1 : second derivative: 6*xhat^3/(40^2
-        pval = fmincon(@(x) ((1/2)*x'*H*x + fnew*x), [(30/40^2)*(mean(xval))^4;mean(diff(yval)./diff(xval));fntofit(mean(xval))],[],[],[],[],[.01;-250;-250],[],@(x)fnnonlincon(x,xsamplePts,fnvalatxsamplepts),opt1);
-        %   =                 fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon)
-        
-        %{
-x = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon)
-		pval = quadprog(H,f,[],[],[],[],[-200;-200;0],[]);
-		Q = [0 0 0;...
-				 0 pval(3) pval(2)/2;...
-				 0 pval(2)/2 pval(1)];
-%}
+		pval = fmincon(@(x) ((1/2)*x'*H*x + fnew*x), [(30/40^2)*(mean(xval))^4;mean(diff(yval)./diff(xval));fntofit(mean(xval))],...
+					[],[],[],[],[.01;-250;-250],[],@(x)fnnonlincon(x,xsamplePts,fnvalatxsamplepts),opt1);
 		quadcell{k} = [0 0 0;...
 									0 pval(1) pval(2)/2;...
 									0 pval(2)/2 pval(3)];
-
-		%{
-		figure;
-        plot(xval,fntofit(xval),'b--');
-        hold on ;
-        yvalmin = polyval(quadcell{k},xval);
-        plot(xval, yvalmin,'r-.');
-	%}	
+	end % of for loop
 
 
-
-	end
-
-
-end
+end % of fn fitForFnSq
 
 
 function [c,ceq] = fnnonlincon(x,xsamplePts,fnvalatxsamplepts)
-% enhancements: return the gradients analytically
-		ceq = 0;
-%         keyboard;
+% the nonlinear constraint is that the quadratic must always lie above the function being fitted in the zone of interst.
+% TODO: enhancements: return the gradients analytically
+
+	ceq = 0;
 		c = max(fnvalatxsamplepts - polyval(x,xsamplePts));
 
-
-
-end
+end % of fn fnnonlincon
